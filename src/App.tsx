d83ce9926229bf9958bd28af8a7da93bc187d86f
eweseongyeoh <eweseongyeoh@airasia.com>
2021-01-22 21:23:29 +0800

    Setup main app with components to form dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import EmployeeAnalyses from 'components/EmployeeAnalyses';
+import EmployeeTable from 'components/EmployeeTable';
+import EmployeeTally from 'components/EmployeeTally';
+import {
+  getHighestEarningEmployee,
+  getMostRecentlyJoinedEmployee,
+} from 'helpers/employeeHelper';
+import { orderBy } from 'lodash';
+import React, { useCallback, useState } from 'react';
+import EmployeeService from 'services/employeeService';
+
+const ASC = 'asc';
+const DESC = 'desc';
 
 function App() {
+  const [employees] = useState<Employee[]>(EmployeeService.getEmployees());
+  const [dateJoinedOrder, setDateJoinedOrder] = useState<'asc' | 'desc'>(DESC);
+
+  const toggleDateJoinedOrder = useCallback(() => {
+    setDateJoinedOrder(dateJoinedOrder === ASC ? DESC : ASC);
+  }, [dateJoinedOrder]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="employee-dashboard container">
+      <div className="row align-items-center py-3">
+        <div className="col-12 col-sm-4 col-md-2">
+          <EmployeeTally tally={employees.length} />
+        </div>
+        <div className="col-12 col-sm-8 col-md-10">
+          <EmployeeAnalyses
+            analyses={{
+              'Highest earning employee': getHighestEarningEmployee(employees),
+              'Employee most recently joined': getMostRecentlyJoinedEmployee(
+                employees
+              ),
+            }}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <EmployeeTable
+            employees={orderBy(employees, ['dateJoined'], [dateJoinedOrder])}
+            toggleDateJoinedOrder={toggleDateJoinedOrder}
+          />
+        </div>
+      </div>
     </div>
   );
 }
